35aa5de1fa4ea3a83ed49b20af2bf50b8c3dc8fc
oceanquest <oqpratik@gmail.com>
2025-10-14 07:55:20 +0545

    Bulk files added for starter repo
    Bulk files added for starter repo

---
+++ awe/ShopProductWriter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace awe;
+
+abstract class ShopProductWriter
+{
+    protected $products = [];
+    
+    public function addProduct(ShopProduct $shopProduct)
+    {
+        $this->products[] = $shopProduct;
+    }
+
+    public function setProducts($products){
+        $this->products=$products;
+    }
+
+    abstract public function write();
+
+}
